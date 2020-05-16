7b6a05c653950f9e217290c381c051a1a90905fa
RadioJackie <jacobpeters628@gmail.com>
2020-05-16 16:58:38 -0700

    Set up all the files and made a search bar

---
+++ index.js
@@ -0,0 +1,7 @@
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name')
+var description = document.querySelector('.description')
+var temp = document.querySelector('.temp')
+
+fetch()
