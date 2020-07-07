27e6399f3318c9d679c66725267c7778ce0da10a
RadioJackie <jacobpeters628@gmail.com>
2020-07-06 21:55:42 -0700

    Not sure

--- index.js
+++ index.js
@@ -4,4 +4,28 @@ var name = document.querySelector('.name')
 var description = document.querySelector('.description')
 var temp = document.querySelector('.temp')
 
-fetch()
+button.addEventListener("click", function () {
+    $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/forecast?q={Phoenix}&appid={8f9da299e9eabd5bd1bb18caeac625e8'
+
+        ,method: "GET"
+        
+        .then(response => response.json())
+            .then(data => {
+                var nameValue = data['name']
+                var temp = data['main']['temp']
+                var descriptionValue = data['weather'][0]['description'];
+
+                name.innnerHTML = nameValue;
+                temp.innnerHTML = tempValue;
+                description.innerHTML = descriptionValue;
+            })
+    })
+
+
+
+
+        .catch(err => alert("Wrong name"))
+
+})
+
