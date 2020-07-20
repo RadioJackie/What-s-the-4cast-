6380b4cd097dfc823043a0298989f419cb5b4013
RadioJackie <jacobpeters628@gmail.com>
2020-07-20 16:37:40 -0700

    Done but the temp is off

--- index.js
+++ index.js
@@ -1,31 +1,94 @@
-var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
-var name = document.querySelector('.name')
-var description = document.querySelector('.description')
-var temp = document.querySelector('.temp')
 
-button.addEventListener("click", function () {
+
+let cities = [""];
+
+function displayWeatherInfo(city) {
+    let APIKey = '554f896fa7e371698189deb9b4b589d9';//Tested key. Still working
+
+    let queryCurrentURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
+    let queryForecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIKey;
+
+    //Request and response for current temperature, humidity and wind speed.
     $.ajax({
-        url: 'https://api.openweathermap.org/data/2.5/forecast?q={Phoenix}&appid={8f9da299e9eabd5bd1bb18caeac625e8'
+        url: queryCurrentURL,
+        method: "GET"
+    }).then(function (response) {
+        let dataDiv = $("#data");
+        dataDiv.html("<h1>" + response.name + ' (' + moment().format('M/D/YYYY') + ')' + "<img src='http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png'>" + "</h1>");
+        dataDiv.append("<p>Wind Speed: " + response.wind.speed + "</p>");
+        dataDiv.append("<p>Humidity: " + response.main.humidity + "</p>");
+        dataDiv.append("<p>Temperature: " + response.main.temp + ' F</p>');
 
-        ,method: "GET"
+        let lon = response.coord.lon;
+        let lat = response.coord.lat;
+        let queryUVUrl = 'https://api.openweathermap.org/data/2.5/uvi?appid=' + APIKey + '&lat=' + lat + '&lon=' + lon;
+
+        // Request and response for uv index data
+        $.ajax({
+            url: queryUVUrl,
+            method: "GET"
+        }).then(function (response) {
+            dataDiv.append("<p>UV Index: " + response.value + "</p>");
+        });
         
-        .then(response => response.json())
-            .then(data => {
-                var nameValue = data['name']
-                var temp = data['main']['temp']
-                var descriptionValue = data['weather'][0]['description'];
+        // Request and response for 5day forecast
+        $.ajax({
+            url: queryForecastURL,
+            method: "GET"
+        }).then(function (response) {
+            for (let i = 6; i < response.list.length; i += 8) {
 
-                name.innnerHTML = nameValue;
-                temp.innnerHTML = tempValue;
-                description.innerHTML = descriptionValue;
-            })
-    })
+                let day = $("#day");
+                day.prepend('<h4>' + response.list[i].dt_txt + '</h4>');
+                day.prepend("<img src='http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + ".png'>");
+                day.prepend('<p>Temp: ' + response.list[i].main.temp + " F</p>");
+                day.prepend('<p>Humidity: ' + response.list[i].main.humidity + ' %</p>');
 
 
+            };
+        });
 
 
-        .catch(err => alert("Wrong name"))
+    });
 
-})
 
+};
+
+
+//Function for the render button
+function renderButtons(city) {
+    //$(".citiesDump").empty();
+
+    let btn = $("<button>");//Dynamically adding the button
+
+    btn.addClass("city-btn");//Adding a class to the button
+
+    btn.attr("data-name", city);//Adding a data-attribute
+
+    btn.text(city);//Adding text to the button
+
+    $(".citiesDump").append(btn);//Inserts the button to the page
+
+};
+
+
+//When the search button is pressed this functions will be executed 
+let searchBtn = $('#searchBtn');
+
+searchBtn.on("click", function (event) {//the on click function
+    event.preventDefault();//So the form does not refresh the page
+
+    let city = $("#cityInput").val();//Get the input value from the user
+
+    cities.push(city);//Push the value, aka city, into the array.
+
+    renderButtons(city);//Execute the render button function with the value passed in
+
+    displayWeatherInfo(city);//Execute the display info function with the value passed in
+});
+
+//Function to attach the data to the added city button
+$(document).on("click", ".city-btn", function () {
+    let city = $(this).attr("data-name");
+    displayWeatherInfo(city);
+});
